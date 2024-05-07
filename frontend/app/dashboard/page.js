47c5214c3a725a66187689f16216d572a38153f0
Anas <anasolin01777@gmail.com>
2024-05-07 14:51:36 +0600

    dashboard todo update complete

--- frontend/app/dashboard/page.js
+++ frontend/app/dashboard/page.js
@@ -14,6 +14,25 @@ import { useRouter } from "next/navigation";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { ToastContainer, toast } from "react-toastify";
 import { activeUser } from "../redux/userSlice";
+import BorderColorIcon from "@mui/icons-material/BorderColor";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 300,
+  height: 200,
+  bgcolor: "background.paper",
+  border: "2px solid #fff",
+  boxShadow: 24,
+  p: 4,
+};
 
 export default function DenseTable() {
   const user = useSelector((state) => state.user.user);
@@ -21,18 +40,68 @@ export default function DenseTable() {
     if (user.role == "user") {
       router.push("/todos");
     }
-  });
+  }, []);
   const router = useRouter();
-  const [todo, setTodo] = React.useState();
+  const [todos, setTodos] = React.useState([]);
+  const [todo, setTodo] = React.useState("");
+  const [todoId, setTodoId] = React.useState("");
+  const [des, setDes] = React.useState("");
   const [update, setUpdate] = React.useState(false);
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = (e) => {
+    setTodo(e.todo);
+    setDes(e.des);
+    setTodoId(e._id);
+    console.log(e);
+    setOpen(true);
+  };
+  const handleClose = () => setOpen(false);
   const dispatch = useDispatch();
+
+  const handleUpdateSubmit = () => {
+    axios
+      .post("http://localhost:8000/edit", {
+        todo: todo,
+        des: des,
+        id: todoId,
+      })
+      .then((res) => {
+        setTodo("");
+        setDes("");
+        setTodoId("");
+        setOpen(false);
+        setUpdate((update) => !update);
+        toast.success(res.data, {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+      })
+      .catch((err) => {
+        setOpen(false);
+        toast.error(err.response.data, {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+      });
+  };
 
   React.useEffect(() => {
     axios
       .get("http://localhost:8000/alltodos")
       .then((res) => {
-        setTodo(res.data);
-        console.log(res.data);
+        setTodos(res.data);
       })
       .catch((err) => {});
   }, [update]);
@@ -72,11 +141,43 @@ export default function DenseTable() {
         });
       });
   };
-  console.log(todo);
+
   return (
     <div className="container m-auto">
       <ToastContainer />
-      {todo && (
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <div className="flex flex-col justify-between h-full">
+            <TextField
+              value={todo}
+              onChange={(e) => setTodo(e.target.value)}
+              label="Todo"
+              id="outlined-size-small"
+              size="small"
+            />
+            <TextField
+              value={des}
+              onChange={(e) => setDes(e.target.value)}
+              label="Des"
+              id="outlined-size-small"
+              size="small"
+            />
+            <Button
+              size="small"
+              onClick={handleUpdateSubmit}
+              variant="contained"
+            >
+              Update
+            </Button>
+          </div>
+        </Box>
+      </Modal>
+      {todos && (
         <TableContainer component={Paper}>
           <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
             <TableHead>
@@ -90,7 +191,7 @@ export default function DenseTable() {
               </TableRow>
             </TableHead>
             <TableBody>
-              {todo.map((item) => (
+              {todos.map((item) => (
                 <TableRow
                   key={item.todo}
                   sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
@@ -115,6 +216,11 @@ export default function DenseTable() {
                     )}
                   </TableCell>
                   <TableCell>
+                    <BorderColorIcon
+                      onClick={() => handleOpen(item)}
+                      sx={{ cursor: "pointer", marginRight: "10px" }}
+                      color="success"
+                    />
                     <DeleteIcon
                       sx={{ cursor: "pointer" }}
                       color="error"
